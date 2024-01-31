780ed51893a7fe549a2189fd406eba398f01cc20
Josh Puglielli <jdpuglielli@gmail.com>
2024-01-31 14:24:35 -0500

    parsed data

--- src_josh/utils.py
+++ src_josh/utils.py
@@ -2,10 +2,11 @@ from transformers import MarianMTModel, MarianTokenizer
 import pandas as pd
 import json
 
-model_name = "Helsinki-NLP/opus-mt-zh-en"
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
 
+##### UNCOMMENT IF NEED TO USE TRANSLATION #####
+# model_name = "Helsinki-NLP/opus-mt-zh-en"
+# tokenizer = MarianTokenizer.from_pretrained(model_name)
+# model = MarianMTModel.from_pretrained(model_name)
 
 def translate_chinese_to_english(text):
     translated = model.generate(**tokenizer(text, return_tensors="pt", padding=True))
@@ -45,8 +46,33 @@ def cognitive_distortion_translate(
 
             
     df = pd.DataFrame(data)
-    df.to_csv("data_josh/cognitive_distortion_train.csv", index=False)
+    df.to_csv("data_josh/cd_train_eng.csv", index=False)
+    
+DISTORTIONS = ["Black and White", "Overgeneralization", "Mental Filtering", "Disqualifying the Positive", "Mind Reading", "Fortune Telling", "Catastrophizing", "Emotional Reasoning", "Should Statements", "Labeling", "Self-Blame", "Blaming Others"]    
+def cognitive_distortion_generate_labels(path_to_file="data_josh/cognitive_distortion_train.jsonl"):
+    data = []
+    with open(path_to_file, "r") as file:
+        for line in file:
+            values = json.loads(line)["messages"][2]["content"].split("|")
+            assert len(values) == 37
+            values = [int(x) for x in values[-12:]]
+            assert len(values) == len(DISTORTIONS)
+            data.append(values)
+    df = pd.DataFrame(data, columns=DISTORTIONS)
+    df.to_csv("data_josh/cd_labels.csv", index=False)
 
+    
+def concatonate_data():
+    """
+    Concatonate the data from the two files into one file
+    """
+    df1 = pd.read_csv("data_josh/cd_labels.csv")
+    df2 = pd.read_csv("data_josh/cd_train_eng.csv")
+    df2 = df2.rename(columns={"0": "Thought"})
+    
+    concatenated_df = pd.concat([df2, df1], axis=1)
+    
+    concatenated_df.to_csv("data_josh/cognitive_distortion.csv", index=False)
 
 # Call the function
-cognitive_distortion_translate()
+concatonate_data()
